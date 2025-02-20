1721cde44d56ca0a829719494ce917a09f5bdacc
Vincent LO <vincent@hkweb.org>
2025-02-20 15:44:04 +0800

    update11

--- docs/example.data.js
+++ docs/example.data.js
@@ -1,7 +1,7 @@
 export default {
   async load() {
     // 获取远程数据
-    const response = await fetch('https://books-oftheweek-cityuhk.blogspot.com/feeds/posts/default');
+    const response = await fetch('https://dummyjson.com/posts');
     const data = await response.json();
     return data.posts.slice(0, 3); // Return the first 5 records
     
